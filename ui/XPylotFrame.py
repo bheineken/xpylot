4304fad075e70cbf80ab0777242abf966b4c718e
Florin Epuras <budd.heineken@gmail.com>
2023-11-22 23:52:17 +0200

    Reopened the project

--- ui/XPylotFrame.py
+++ ui/XPylotFrame.py
@@ -5,5 +5,5 @@ class XPylotFrame(ctk.CTk):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.title("XPylot")
-        self.geometry("800x600")
+        self.geometry("1920x1080")
 
